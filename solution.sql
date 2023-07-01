721d21a59eaef0ab03044fb5fe2bb4b589281017
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-07-01 19:19:50 -0400

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,16 +1,22 @@
-Table: Customer
+-- Table: Customer
 
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| id          | int     |
-| name        | varchar |
-| referee_id  | int     |
-+-------------+---------+
-id is the primary key column for this table.
-Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | referee_id  | int     |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
  
 
-Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+-- Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
 
-Return the result table in any order.
+-- Return the result table in any order.
+
+ 
+SELECT name
+FROM Customer
+WHERE referee_id IS NULL OR referee_id <> 2;
+
